380767023462d8a36047637bc227f2cf1bb136e4
anwesh-sarkar <anwesh.sarkar@gmail.com>
2021-05-27 23:09:42 -0400

    Finished flip sort and unit tests

--- flipSort.js
+++ flipSort.js
@@ -1,63 +1,93 @@
 let numOfFlips = 0;
 
-let flip = (arr, k) => {
-  for (let i = 0, j = k; i < k; i++, j--) {
-    [arr[i], arr[j]] = [arr[j], arr[i]];
+// flip the array from index 0 to index k
+let flipArray = (arr, k) => {
+  for (let i = 0; i < k; i++) {
+    let temp = arr[k];
+    arr[k] = arr[i];
+    arr[i] = temp;
+    k--;
     numOfFlips++;
   }
 
+  // stop execution of number of flips > 10 * length of array
   if (numOfFlips > 10 * arr.length) {
-    return;
+    return "Invalid answer. Number of flips > 10 * array.length";
   } else {
     return arr;
   }
 };
 
-let findMaxIndex = (arr, k) => {
-  let max = -Infinity;
-  let maxIndex = 0;
+// find index of max value in the array
+let findIndexOfMaxValue = (arr, k) => {
+  let maxValue = 0;
+  let maxValueIndex = 0;
 
   for (let i = 0; i < k; i++) {
-    if (arr[i] >= max) {
-      max = arr[i];
-      maxIndex = i;
+    if (arr[i] >= maxValue) {
+      maxValue = arr[i];
+      maxValueIndex = i;
     }
   }
 
-  return maxIndex;
+  return maxValueIndex;
 };
 
+// check that every element in array is >= 1 AND is <= length of array
 let checkElemConstraint = (arr) => {
-  for (let i = 0; i <= arr.length; i++) {
-    if (arr[i] < 1 || arr[i] > arr.length) {
-      console.log("inside if");
-      break;
-    } else {
-      console.log("inside else");
-      return arr;
-    }
+  function threshold(currentValue) {
+    return currentValue >= 1 && currentValue <= arr.length;
   }
+
+  let checkThreshold = arr.every(threshold);
+
+  return checkThreshold;
 };
 
+// check that array has no duplicate values
+let checkHasDuplicate = (arr) => {
+  let checkSameValue = arr.some(
+    (currentValue, i) => arr.indexOf(currentValue) !== i
+  );
+
+  return checkSameValue;
+};
+
+// check that array is not empty
+let checkLengthOfArray = (arr) => {
+  let result;
+  if (arr.length >= 1 && arr.length <= 100) {
+    result = true;
+  } else {
+    result = false;
+  }
+  return result;
+};
+
+//perform flip sort
 var flipSort = function (arr) {
-  let i = arr.length;
   let output = [];
 
-  while (1 <= i && i <= 100) {
-    if (checkElemConstraint(arr)) {
-      let maxIndex = findMaxIndex(arr, i);
+  if (checkElemConstraint(arr) === false) {
+    return "Invalid Array. An element is either < 1 OR > array length";
+  } else if (checkHasDuplicate(arr) === true) {
+    return "Invalid Array. Array has duplicate elements";
+  } else if (checkLengthOfArray(arr) === false) {
+    return "Invalid Array. Is empty";
+  } else {
+    for (let i = arr.length; 1 <= i && i <= 100; i--) {
+      let maxValueIndex = findIndexOfMaxValue(arr, i); // find the index of the max value in array. First iteration, index of 4 in [3,2,4,1] is 2
 
-      if (maxIndex !== i - 1) {
-        flip(arr, maxIndex);
-        flip(arr, i - 1);
-        output.push(maxIndex + 1);
-        output.push(i);
+      // check if index of max value is not the same as position in the array. First iteration, check that max value = 4 is not at index = 3 (or position 4)
+      if (maxValueIndex !== i - 1) {
+        flipArray(arr, maxValueIndex); // First iteration, flip the array from 0 to maxValueIndex = 2.  [3,2,4,1] will be flipped to [4,2,3,1]
+        flipArray(arr, i - 1); // First iteration, flip the array from to 0 to last index = 3. [3,2,4,1] will be flipped to [1,3,2,4]
+        output.push(maxValueIndex + 1); // Push value of position of max value = 3.
+        output.push(i); // Push value of length of array which is same as value being sorted
       }
     }
-    i--;
   }
-  console.log(arr);
   return output;
 };
 
-console.log(flipSort([3, 5, 0]));
+module.exports = flipSort;
---
+++ unit-test/flipSort.test.js
@@ -0,0 +1,59 @@
+const flipSort = require("../flipSort");
+
+describe("flip sorting of [3,2,4,1] should produce [3,4,2,3,1,2,2,4,1]", () => {
+  test("flip sort of [3,2,4,1]", () => {
+    expect(flipSort([3, 2, 4, 1])).toEqual([3, 4, 2, 3, 1, 2]);
+  });
+});
+
+describe("flip sorting of [1,2,3,4] should produce []", () => {
+  test("flip sort of [1,2,3,4]", () => {
+    expect(flipSort([1, 2, 3, 4])).toEqual([]);
+  });
+});
+
+describe("invalid element less than 1 thrown on flip sorting [0,2,1]", () => {
+  test("flip sort of [0,2,1]", () => {
+    expect(flipSort([0, 2, 1])).toEqualCaseInsensitive(
+      "Invalid Array. An element is either < 1 OR > array length"
+    );
+  });
+});
+
+describe("invalid element less than 1 thrown on flip sorting [1,0.2,3]", () => {
+  test("flip sort of [1,0.2,3]", () => {
+    expect(flipSort([1, 0.2, 3])).toEqualCaseInsensitive(
+      "Invalid Array. An element is either < 1 OR > array length"
+    );
+  });
+});
+
+describe("invalid element less than 1 thrown on flip sorting [2,-1,3]", () => {
+  test("flip sort of [2,-1,3]", () => {
+    expect(flipSort([2, -1, 3])).toEqualCaseInsensitive(
+      "Invalid Array. An element is either < 1 OR > array length"
+    );
+  });
+});
+
+describe("invalid element greater than array length thrown on flip sorting [2,5,1,3]", () => {
+  test("flip sort of [2,5,1,3]", () => {
+    expect(flipSort([2, 5, 1, 3])).toEqualCaseInsensitive(
+      "Invalid Array. An element is either < 1 OR > array length"
+    );
+  });
+});
+
+describe("duplicate element error thrown on flip sorting [1,2,2]", () => {
+  test("flip sort of [1,2,2]", () => {
+    expect(flipSort([1, 2, 2])).toEqualCaseInsensitive(
+      "Invalid Array. Array has duplicate elements"
+    );
+  });
+});
+
+describe("empty array error thrown on flip sorting []", () => {
+  test("flip sort of []", () => {
+    expect(flipSort([])).toEqualCaseInsensitive("Invalid Array. Is empty");
+  });
+});
